09959189d873cae8c3ee287ed268e85a81c25dda
Devendra Pandey <devendrapandey125@gmail.com>
2021-04-01 10:18:36 +0530

    changes

--- book_exercise/crash_course/2.5_famous_quote.py
+++ book_exercise/crash_course/2.5_famous_quote.py
@@ -1,3 +1,8 @@
 #date: 30/03/2021; quote of a famous person.
 print('Albert Einstein once said, “A person who never made a' + '\n' + 'mistake never tried anything new.”')
 print('Albert Einstein once said, “A person who never made a \nmistake never tried anything new.”')
+
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+print('Albert Einstein once said, “A person who never made a \nmistake never tried anything new."')
+
+print('Albert Einstein once said: \n\t“A person who never made a mistake never tried anything new."')

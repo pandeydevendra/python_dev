395830632bb3b75ddf78da6be7cb8bdfd6049850
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-04-10 18:03:59 +0530

    Dev (#17)
    * list completed
    
    * list apllication done
    
    * list chapter 4
    
    * yml file correction
    
    * Update python-package.yml
    
    * chapter 5_1
    
    * pass files abcd
    
    * chapter5: Qs 1 and 2
    
    * if_elif_else
    
    * chapter_5 Aliens
    
    * chapter_5 complete
    
    * dictionaries

--- DP_exercise/abcd_cound.py
+++ DP_exercise/abcd_cound.py
@@ -1,4 +1,23 @@
+"""
 abcd_suffle = 'aabbbccddd'
 #a1a2b1b2c1c2d1d2d3==a2b2c2d3
 for alphabet in abcd_suffle:
    pass
+"""
+s = "aabbacccdccc"
+lilist_char = []
+list_char.append(s[0])
+#print(list_char)
+s2 = ""
+for i in range(1, len(s)):
+   char = s[i]
+   if s[i] in list_char:
+      list_char.append(char)
+   else:
+      length = len(list_char)
+      last_ele = list_char[-1]
+      s2 = "{}{}{}".format(s2, last_ele, length)
+      list_char = []
+      list_char.append(char)
+
+print(s2)

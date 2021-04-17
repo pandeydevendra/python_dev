522542affae6339821cd275bf12c0367462b62c5
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-04-17 09:27:56 +0530

    dev (#24)
    * pushed_on_main (#23)
    
    * dev (#21)
    
    * list completed
    
    * list apllication done
    
    * list chapter 4
    
    * yml file correction
    
    * Update python-package.yml
    
    * chapter 5_1
    
    * pass files abcd
    
    * chapter5: Qs 1 and 2
    
    * if_elif_else
    
    * chapter_5 Aliens
    
    * chapter_5 complete
    
    * dictionaries
    
    * conflict list_abcd
    
    * mysql cients
    
    * conflict with is & ==
    
    * Folder's name lowercased
    
    * matplotlib.pyplot_001
    
    * graphs and bar_chart
    
    * seaborn installed
    
    * new.py file updated

--- ds_ml/num_py/new.py
+++ ds_ml/num_py/new.py
@@ -11,11 +11,35 @@ df = df_monthly[['datum', 'total']]
 year_list = df.datum
 total_list = df.total
 
-#/home/vins/Workspace/Py/data_files
 df_monthly = pd.read_csv('/home/vins/Workspace/Py/data_files/salesmonthly.csv')
-df_monthly["total"] = df_monthly["M01AB"] + df_monthly["M01AE"] + df_monthly["N02BA"] + df_monthly["N02BE"] + df_monthly["N05B"] + df_monthly["N05C"] + df_monthly["R03"] + df_monthly["R06"]
-#df_monthly.to_excel("/home/vins/Workspace/Py/data_files/monthly_sales_v1.xlsx")
+df_monthly["total"] = df_monthly["M01AB"] + df_monthly["M01AE"] + df_monthly["N02BA"] + df_monthly["N02BE"] + \
+                      df_monthly["N05B"] + df_monthly["N05C"] + df_monthly["R03"] + df_monthly["R06"]
+# df_monthly.to_excel("/home/vins/Workspace/Py/data_files/monthly_sales_v1.xlsx")
 df = df_monthly[['datum', 'total']]
+
+year_list = df.datum
+total_list = df.total
+
+# print(year_list)
+# print(total_list)
+
+plt.xlabel('year_list')
+plt.ylabel('total_list')
+
+plt.plot(year_list, total_list, color='red', marker='*')
+plt.show()
+# df
+
+# fig, axes = plt.subplots(8, 1, figsize=(10, 30), sharex=True)
+# for name, ax in zip(['M01AB','M01AE','N02BA','N02BE', 'N05B','N05C','R03','R06'], axes):
+#  sns.boxplot(data=df_daily, x='Month', y=name, ax=ax)
+
+# df
+
+# fig, axes = plt.subplots(8, 1, figsize=(10, 30), sharex=True)
+# for name, ax in zip(['M01AB','M01AE','N02BA','N02BE', 'N05B','N05C','R03','R06'], axes):
+#  sns.boxplot(data=df_daily, x='Month', y=name, ax=ax)
+
 year_list=df.datum
 total_list=df.total
 
@@ -37,5 +61,4 @@ plt.show()
 
 #fig, axes = plt.subplots(8, 1, figsize=(10, 30), sharex=True)
 #for name, ax in zip(['M01AB','M01AE','N02BA','N02BE', 'N05B','N05C','R03','R06'], axes):
-  #  sns.boxplot(data=df_daily, x='Month', y=name, ax=ax)
-
+  #  sns.boxplot(data=df_daily, x='Month', y=

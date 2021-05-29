ef2cc85a386ebad952b86cb714640a23dc44bb03
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-05-29 14:37:52 +0530

    lms_db (#46)

--- projects/lms/dev_db.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-
-# Open database connection
-db = MySQLdb.connect("localhost", "root", "root@123", "dev_db")
-
-# prepare a cursor object using cursor() method
-#cursor = db.cursor()  # it will give tuple
-cursor = db.cursor(MySQLdb.cursors.DictCursor)  # it will give dict..
--- projects/lms/manage_books.py
+++ projects/lms/manage_books.py
@@ -1,70 +1,6 @@
 from prettytable import PrettyTable
-from dev_db import cursor, db
 
-cur = cursor
-mydb = db
-
-
-def get_book_id(b_title):
-    """
-    input: str
-    return: tuple (int, dict)
-    """
-    sql_query = "SELECT * FROM books WHERE title = '{}'".format(b_title)
-    print(sql_query)
-    cur.execute(sql_query)
-    book = cur.fetchone()
-    if book:
-        book_id = book["id"]
-        book_info = PrettyTable()
-        book_info.field_names = ["id", "title", "price", "total", "available"]
-        book_data = [book["id"], book['title'], book['price'], book['total'], book['available']]
-        book_info.add_row(book_data)
-        print(book_info)
-
-    else:
-        book_id = 0
-    return book_id, book
-
-
-def add_book(b_title, b_price, b_total, b_available):
-    """
-    this fn is use to add records in book table
-    """
-    print(b_title, b_price, b_total, b_available)
-    insert_in_book = "INSERT INTO books(title, price, total, available) VALUES(%s,%s,%s,%s)"
-    book = (b_title, b_price, b_total, b_available)
-    cur.execute(insert_in_book, book)
-    mydb.commit()
-
-
-def update_book(b_id, updated_title, new_price, updated_total, updated_available):
-    """
-    this function is use to update records in book table
-    """
-    q_sql_update_table = "UPDATE books SET title = '{}', price = '{}', total = '{}', available = '{}'" \
-                         " WHERE id = '{}'".format(
-        updated_title, new_price, updated_total, updated_available, b_id
-    )
-    print(q_sql_update_table)
-    cur.execute(q_sql_update_table)
-    mydb.commit()
-
-
-def delete(b_id):
-    q_sql_del_table = "DELETE FROM books WHERE id = '{}'".format(b_id)
-    print(q_sql_del_table)
-    cur.execute(q_sql_del_table)
-    mydb.commit()
-
-
-def select():
-    sql_query = "SELECT * FROM books"
-    cur.execute(sql_query)
-    books = cur.fetchall()
-    print(books)
-    print(type(books))
-    return books
+import database as lms_db
 
 
 def add_book_process():
@@ -79,13 +15,13 @@ def add_book_process():
     book_price = float(input("Enter the book's price: "))
     book_total = int(input("Enter total copies of the book: "))
     book_available = book_total
-    add_book(book_title.title(), book_price, book_total, book_available)
+    lms_db.add_book(book_title.title(), book_price, book_total, book_available)
 
 
 def select_process():
     books_show = PrettyTable()
     books_show.field_names = ["Title", "Price", "Total", "Available"]
-    books = select()
+    books = lms_db.select_book()
     for book in books:
         book_details = [book['title'], book['price'], book['total'], book['available']]
         books_show.add_row(book_details)
@@ -94,7 +30,7 @@ def select_process():
 
 def update_process():
     current_title = input("Enter the book's current title: ")
-    book_id, _ = get_book_id(current_title)
+    book_id, _ = lms_db.get_book_id(current_title)
     if book_id == 0:
         print("Book titled {} doesn't exist.".format(current_title))
     else:
@@ -102,18 +38,18 @@ def update_process():
         updated_price = float(input("Enter new price: "))
         updated_total = int(input("Enter new total number of copies: "))
         updated_available = int(input("Enter available number of copies: "))
-        update_book(book_id, updated_title, updated_price, updated_total, updated_available)
+        lms_db.update_book(book_id, updated_title, updated_price, updated_total, updated_available)
 
 
 def delete_process():
     book_title = input("Enter the book's title to be deleted: ")
-    book_id, _ = get_book_id(book_title)
+    book_id, _ = lms_db.get_book_id(book_title)
     if book_id == 0:
         print("Book titled {} doesn't exist.".format(book_title))
     else:
         ch = input("Are you sure you want to delete {} book?y/n: ".format(book_title))
         if ch.lower() == 'y':
-            delete(book_id)
+            lms_db.delete_book(book_id)
         else:
             print("Thank you! Your book is safe in our record.")
 
@@ -147,5 +83,5 @@ def start_program():
             print('''Wrong input!! enter valid choice: ''')
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     start_program()

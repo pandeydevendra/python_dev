8d49ab4541284583d96a3615c18b67f88e97bcec
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-05-30 17:37:59 +0530

    smart calculator (#47)

--- core/video_tutorials/mysirg/assignments/calculator/dev_calc.py
+++ core/video_tutorials/mysirg/assignments/calculator/dev_calc.py
@@ -4,23 +4,24 @@ sys.path.append('/calc_module/')
 import calc_module
 from calc_module.math import *
 
-print(responces[0])
-print(responces[1])
+print(responses[0])
+print(responses[1])
 while True:
-    print()
+    print()  # to change line
     text = input("Enter your question/query: ")
     for word in text.split(' '):
-        if word.upper() in operations.key():
+        if word.upper() in operations.keys():
             try:
-                l = extract_numbers_from_text
+                l = extract_numbers_from_text(text)
                 r = operations[word.upper()](l[0], l[1])
                 print(r)
-            except:
-                print("Some Error")
+            except Exception as e:
+                print("Some Error", e)
             finally:
                 break
-        #elif word.upper() in commands.keys()
+        elif word.upper() in commands.keys():
+            commands[word.upper()]()
+            break
+
         else:
             sorry()
-
-# TODO // calculator implementation

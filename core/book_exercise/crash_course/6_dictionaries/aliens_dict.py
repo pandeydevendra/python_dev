19798d5b631055333101c006001d9d71177a8bf6
Devendra Pandey <devendrapandey125@gmail.com>
2021-06-20 18:02:16 +0530

    dictionary

--- core/book_exercise/crash_course/6_dictionaries/aliens_dict.py
+++ core/book_exercise/crash_course/6_dictionaries/aliens_dict.py
@@ -41,7 +41,7 @@ print("New x-position: " + str(alien_0['x_position']))
 # speed : slow;
 alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'slow'}
 print("\nOriginal x-position: " + str(alien_0['x_position']))
-print("speed is ", alien_0['speed'])
+print("speed is", alien_0['speed'])
 # Move the alien to the right.
 # Determine how far to move the alien based on its current speed.
 if alien_0['speed'] == 'slow':
@@ -56,7 +56,7 @@ print("New x-position: " + str(alien_0['x_position']))
 
 alien_0['speed'] = 'fast'
 print("\nOriginal x-position: " + str(alien_0['x_position']))
-print("speed is ", alien_0['speed'])
+print("speed is", alien_0['speed'])
 if alien_0['speed'] == 'slow':
     x_increment = 1
 elif alien_0['speed'] == 'medium':
@@ -73,3 +73,35 @@ alien_1st = {'color': 'green', 'points': 5}
 print(alien_1st)
 del alien_1st['points']
 print(alien_1st)
+
+# part 2:
+lien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+print("")
+# Make an empty list for storing aliens.
+aliens = []
+# Make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:15]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+# Show the first 5 aliens:
+for alien in aliens[:11]:
+    print(alien)
+print("...")
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
--- core/book_exercise/crash_course/6_dictionaries/favorite_languages.py
+++ core/book_exercise/crash_course/6_dictionaries/favorite_languages.py
@@ -50,3 +50,31 @@ for name in favorite_languages.keys():
 
 for name in sorted(favorite_languages.keys()):
     print(name.title() + ", thank you for taking the poll.")
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+print("\nThe following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+print("\n")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+# set() function gives distinct value
+
+# List inside Dictionary::::
+
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+}
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())

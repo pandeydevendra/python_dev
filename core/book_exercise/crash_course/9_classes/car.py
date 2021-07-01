bd3f434960d66124c1d6667d2d19cfac3da44b42
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-07-01 18:05:06 +0530

    class (#67) (#68)
    classes
    
    classes
    
    car class model
    
     car model
    
    class user

---
+++ core/book_exercise/crash_course/9_classes/car.py
@@ -0,0 +1,60 @@
+class Car():
+    """Asimple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        """Add an attribute that stores the car’s overall mileage."""
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car mileage."""
+        return f"This car has {str(self.odometer_reading)} miles on it."
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value.
+         Reject any change in odometer setting."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            return f"You can't roll back an odometer."
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+print(my_new_car.read_odometer())
+
+"""Modifying an Attribute’s Value Directly."""
+
+my_new_car.odometer_reading = 23
+print(my_new_car.read_odometer())
+
+"""Modifying an Attribute’s Value Through a Method."""
+
+my_new_car.update_odometer(45)
+print(my_new_car.read_odometer())
+
+"""Reject any change in odometer setting."""
+
+my_new_car.update_odometer(20)
+print(my_new_car.read_odometer())
+
+"""Incrementing an Attribute’s Value Through a Method."""
+print()
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23500)
+print(my_used_car.read_odometer())
+my_used_car.increment_odometer(100)
+print(my_used_car.read_odometer())
--- core/book_exercise/crash_course/9_classes/user.py
+++ core/book_exercise/crash_course/9_classes/user.py
@@ -3,12 +3,23 @@ class User():
     def __init__(self, f_name, l_name):
         self.first_name = f_name
         self.last_name = l_name
+        """Add an attribute login attempt."""
+        self.login_attempt = 0
 
     def describe_user(self):
         print(f"User's name is {self.first_name.title()} {self.last_name.title()}.")
 
     def greet_user(self):
         print(f"Welcome Mr. {self.first_name.title()}.")
+
+    def count_user_login(self):
+        print(f"{self.first_name.title()} has attempted to login {str(self.login_attempt)} times.")
+
+    def increment_login_attempts(self):
+        self.login_attempt += 1
+
+    def reset_login_attempts(self):
+        self.login_attempt = 0
 
 
 dev = User('Devendra', 'Pandey')
@@ -17,3 +28,21 @@ dev.describe_user()
 dev.greet_user()
 vin.describe_user()
 vin.greet_user()
+
+dev.count_user_login()
+dev.increment_login_attempts()
+dev.count_user_login()
+dev.increment_login_attempts()
+dev.increment_login_attempts()
+dev.increment_login_attempts()
+dev.count_user_login()
+
+dev.reset_login_attempts()
+dev.count_user_login()
+
+vin.count_user_login()
+vin.reset_login_attempts()
+vin.count_user_login()
+vin.increment_login_attempts()
+vin.increment_login_attempts()
+vin.count_user_login()

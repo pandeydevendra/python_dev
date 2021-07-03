5759d98fd5bb10551b2c527e5a7419cfca117759
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-07-03 12:07:29 +0530

    classes (#70)

--- core/book_exercise/crash_course/9_classes/my_car.py
+++ core/book_exercise/crash_course/9_classes/my_car.py
@@ -1,5 +1,8 @@
 # Importing a Single Class;
 from car import Car
+from electric_car import Car
+from electric_car import Car, ElectricCar
+import electric_car
 
 my_new_car = Car('audi', 'a001', 2016)
 print(my_new_car.get_descriptive_name())
@@ -8,7 +11,7 @@ print(my_new_car.read_odometer())
 
 print()
 
-from electric_car import Car
+# from electric_car import Car
 
 my_new_car = Car('audi', 'a007', 2020)
 print(my_new_car.get_descriptive_name())
@@ -19,7 +22,7 @@ print()
 
 # Importing Multiple Classes from a Module::
 
-from electric_car import Car, ElectricCar
+# from electric_car import Car, ElectricCar
 
 my_beetle = Car('volkswagen', 'beetle', 2016)
 print(my_beetle.get_descriptive_name())
@@ -30,7 +33,7 @@ print()
 
 # Importing an Entire Module:;
 
-import electric_car
+# import electric_car
 
 my_beetle = electric_car.Car('volkswagen01', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())

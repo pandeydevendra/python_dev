527badb1b18a26eb5c1d3fd3737924bda1c1e990
Devendra Pandey <devendrapandey125@gmail.com>
2021-10-02 11:34:57 +0530

    data downloading and python revision
    numpy data correction
    
    large file size removed
    
    data downloading and python revision
    
    rebase

--- ds_ml/ds/udacity/account.py
+++ ds_ml/ds/udacity/account.py
@@ -1,3 +1,10 @@
+import unicodecsv
+
+def read_csv(filename):
+    with open(filename, 'rb') as f:
+        reader = unicodecsv.DictReader(f)
+        return list(reader)
+
 def get_unique_students(data):
     unique_students = set()
     for data_point in data:
@@ -5,12 +12,16 @@ def get_unique_students(data):
     return unique_students
 
 
+enrollments = read_csv('enrollments.csv')
+daily_engagement = read_csv('daily_engagement.csv')
+project_submissions = read_csv('project_submissions.csv')
+
 len(enrollments)
 unique_enrolled_students = get_unique_students(enrollments)
 len(unique_enrolled_students)
 len(daily_engagement)
-unique_engagement_students = get_unique_students(daily_engagement)
-len(unique_engagement_students)
+# unique_engagement_students = get_unique_students(daily_engagement)
+# len(unique_engagement_students)
 len(project_submissions)
 unique_project_submitters = get_unique_students(project_submissions)
 len(unique_project_submitters)

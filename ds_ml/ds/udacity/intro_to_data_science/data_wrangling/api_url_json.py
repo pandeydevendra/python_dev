9c832aca49b43c0a7a41eafa89bd22e75edd18b2
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-12-01 20:42:15 +0530

    rest api for data collection (#86)
    rest api for data collection

--- ds_ml/ds/udacity/intro_to_data_science/data_wrangling/api_url_json.py
+++ ds_ml/ds/udacity/intro_to_data_science/data_wrangling/api_url_json.py
@@ -8,10 +8,20 @@ def api_get_request(url):
     # the function; you do not need to construct the address or call this
     # function in your grader submission.
     #
+
+    data_url = requests.get(url).text
+    data = json.loads(data_url)
+
     # Once you've done this, return the name of the number 1 top artist in
     # Spain.
+    spain_artist = data['topartists']['artist'][0]['name']
+    # return the top artist in Spain
 
-    return ...  # return the top artist in Spain
+    return spain_artist  # return the top artist in Spain
 
 
-api_get_request('url') # write right url
+url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=4beab33cc6d65b05800d51f5e83bde1b&format=json'
+top_artist = api_get_request(url)
+print(top_artist)
+
+#  Good job! Arctic Monkeys is top artist in Spain

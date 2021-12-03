2543b68c3546a3d3dbb22c22422a9166baf50fb6
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2021-12-04 00:19:39 +0530

    pandas in data science (#88)

---
+++ ds_ml/pandas/codebasics_pandas/Readme.md
@@ -0,0 +1,8 @@
+Different ways of creating 'pandas- dataframe':
+1) Using 'CSV'
+2) Using 'Excel'
+3) From python 'dictionary'
+4) From list of 'tuples'
+5) From list of 'dictionary'
+
+
--- ds_ml/pandas/smaple.py
+++ ds_ml/pandas/smaple.py
@@ -1,29 +1,8 @@
-'''
-We'll see why numpy is very popular and talk about its
-main feature "n dimensional array". It is memory efficient,
-fast and convenient compared to python native list.
-h
-
-'''
-import numpy as np
 import pandas as pd
-# import tie
-import sys
 
-
-df = pd.read_csv (r'Path where the CSV file is stored\File name.csv')
-print (df)
-
-l = range(1000)  # create a list with thousand/1000 element in it
-
-print(sys.getsizeof(5) * len(l))  # here 5 is any number between 0-999 // consume 24000 byte
-'''
-size of one python object list is 24 byte (64 byte Architecture)
 '''
 
-numPyarray = np.arange(1000)  # arange is funtion to create array element 0-999 // consume 8000 byte
+'''
 
-print(numPyarray.size * numPyarray.itemsize)
-'''
-size of one  numbpy python object list is 8 byte (64 byte Architecture)
-'''
+df = pd.read_csv(r'/home/vins/Workspace/Py/python/ds_ml/pandas/salary_data.csv')
+print(df)

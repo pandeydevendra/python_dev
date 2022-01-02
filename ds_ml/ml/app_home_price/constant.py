2e12d7832f65d5253734454468f4fbd82ebaae70
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2022-01-02 18:02:13 +0530

    user-friendly homeprice prediction model (#125)

---
+++ ds_ml/ml/app_home_price/constant.py
@@ -0,0 +1,3 @@
+BASE_PATH = "/home/vins/Workspace/Py/python"
+FILE_PATH_CSV = f"{BASE_PATH}/data_files/home_price/data.csv"
+FILE_PATH_PICKLE = f"{BASE_PATH}/data_files/home_price/trained_model.pickle"

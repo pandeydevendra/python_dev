326c284e560695583014e9c4933b5444c2c5e7cd
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2022-01-02 09:59:00 +0530

    variabe name changed (#124)

--- ds_ml/ml/codebasics/2_gradient_descent/gradient_descent.py
+++ ds_ml/ml/codebasics/2_gradient_descent/gradient_descent.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 
 def gradient_descent(x, y):
-    m_curr = b_curr = 0
+    m = b = 0
     iterations = 100
     n = len(x)
     learning_rate = 0.06
     for i in range(iterations):
-        y_predicted = m_curr * x + b_curr
+        y_predicted = m * x + b
         cost = (1 / n) * (sum([val ** 2 for val in (y - y_predicted)]))
         md = -(2 / n) * sum(x * (y - y_predicted))
         bd = -(2 / n) * sum(y - y_predicted)
-        m_curr = m_curr - learning_rate * md
-        b_curr = b_curr - learning_rate * bd
-        print("m {}, b {}, cost {}, iteration {}".format(m_curr, b_curr, cost, i))
+        m = m - learning_rate * md
+        b = b - learning_rate * bd
+        print("m {}, b {}, cost {}, iteration {}".format(m, b, cost, i))
 
 
 x = np.array([1, 2, 3, 4, 5])

b87a94078ae7e73fbc5a66f95ebaaf2ee876f2f0
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2022-01-13 12:45:11 +0530

    Dev (#135)
    * sql update (#132)
    
    name main concept
    
    modified
    
    * sql update (#134)
    
    name main concept
    
    decor practice
    
    poc

--- dp_exercise/second.py
+++ dp_exercise/second.py
@@ -1,15 +1,22 @@
 def f2():
-    print("f2 inside second file")
+    print("f2 inside 2nd file")
 
 
 def f3():
-    print("f3 inside second file")
+    print("f3 inside 3rd file")
 
 
 f3()
 print("ame::::::", __name__)
 if __name__ == '__main__':
     f2()
+
+import second
+
+
+def f1():
+    print("f1 inside first")
+
 
 import second
 

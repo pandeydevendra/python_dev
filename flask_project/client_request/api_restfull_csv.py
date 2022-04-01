a3870fac25acf1b267d70225b82f34f880b63676
Devendra K Pandey <64904189+pandeydevendra@users.noreply.github.com>
2022-04-01 21:30:01 +0530

    restful api (#167) (#168)

---
+++ flask_project/client_request/api_restfull_csv.py
@@ -0,0 +1,39 @@
+from pprintpp import pprint as pp
+
+import requests
+
+url = "http://127.0.0.1:5000/users"
+
+
+def add_user():
+    payload = {'name': 'Sachin2',
+               'age': '32',
+               'city': 'Pune'}
+    files = [
+
+    ]
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    print(response.text)
+
+
+def get_all_user():
+    payload = {'name': 'Sachin',
+               'age': '35',
+               'city': 'Mumbai'}
+    files = [
+
+    ]
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+    pp(response.json())
+
+
+if __name__ == "__main__":
+    # get_all_user()
+    add_user()
+    #

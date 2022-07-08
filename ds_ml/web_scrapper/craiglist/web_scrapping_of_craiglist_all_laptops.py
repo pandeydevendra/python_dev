52c409b620355c6ffcd87e61902e1dc7f1a7886b
Devendra Pandey <devendrapandey125@gmail.com>
2022-07-08 22:59:53 +0530

    file relocation

--- ds_ml/web_scrapper/craiglist/web_scrapping_of_craiglist_all_laptops.py
+++ ds_ml/web_scrapper/craiglist/web_scrapping_of_craiglist_all_laptops.py
@@ -13,6 +13,17 @@ web_db = MySQLdb.connect("localhost", "root", "root@123", "web_scrap_data", char
 # prepare a cursor object using cursor() method
 cursor = web_db.cursor()
 
+FILE_PATH = '/home/vins/Workspace/Py/data_files/web_scrap/craiglist'
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+}
+
 
 def add_laptop_data(id, name, price, date, location, url):
     """
@@ -35,90 +46,93 @@ def add_laptop_data(id, name, price, date, location, url):
     web_db.commit()
 
 
-def main_program(url):
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Cache-Control': 'max-age=0'
-    }
-    base_url = url
-    print(f"current_url : {base_url}")
-
-    response = requests.get(base_url, headers=headers)
+def get_total_pages(base_url):
+    response = requests.get(base_url, headers=HEADERS)
     soup = BeautifulSoup(response.content, 'html.parser')
     total_results = int(soup.find('span', 'totalcount').text)
     print(f"total_results : {total_results}")
     total_pages = (total_results // 120) + 1
     print(f"total_pages : {total_pages}")
+    return total_pages
 
-    search_results = []
 
-    for i in range(0, total_pages):
-        params = {
-            's': i * 120
-        }
+def get_laptop_list(page_number, base_url):
+    search_result_per_page = []
+    params = {
+        's': page_number * 120
+    }
 
-        print(f"params : {params}")
-        print('Processing Page {0}'.format(i + 1))
+    print(f"params : {params}")
+    print('Processing Page {0}'.format(page_number + 1))
 
-        # response = requests.get(base_url, headers=headers, params=params)
-        page_url = f"{base_url}?s={i * 120}"
-        print(f"page_url : {page_url}")
-        response = requests.get(page_url, headers=headers)
+    page_url = f"{base_url}?s={page_number * 120}"
+    print(f"page_url : {page_url}")
+    response = requests.get(page_url, headers=HEADERS)
 
-        # continue
-        soup = BeautifulSoup(response.content, 'html.parser')
+    # continue
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        with open("1.html", 'w') as fp:
-            fp.write(str(soup))
+    with open(f"{FILE_PATH}/1.html", 'w') as fp:
+        fp.write(str(soup))
 
-        results = soup.find('ul', {'id': 'search-results'})
-        with open("2.html", 'w') as fp:
-            fp.write(str(results))
+    results = soup.find('ul', {'id': 'search-results'})
+    with open(f"{FILE_PATH}/2.html", 'w') as fp:
+        fp.write(str(results))
 
-        result_rows = results.find_all('li', 'result-row')
-        with open("3.html", 'w') as fp:
-            fp.write(str(result_rows))
+    result_rows = results.find_all('li', 'result-row')
+    with open(f"{FILE_PATH}/3.html", 'w') as fp:
+        fp.write(str(result_rows))
 
-        laptop_count = 0
-        for result_row in result_rows:
-            with open("4.html", 'w') as fp:
-                fp.write(str(result_row))
+    laptop_count = 0
+    for result_row in result_rows:
+        with open(f"{FILE_PATH}/4.html", 'w') as fp:
+            fp.write(str(result_row))
 
-            laptop_info = {}
-            # import ipdb
-            # ipdb.set_trace()
-            laptop_count += 1
-            # if laptop_count >= 2:
-            #     break
+        laptop_info = {}
+        # import ipdb
+        # ipdb.set_trace()
+        laptop_count += 1
+        # if laptop_count >= 2:
+        #     break
 
-            post_datetime = result_row.time['datetime']
-            laptop_info['post_datetime'] = post_datetime
-            post_id = result_row.h3.a['data-id']
-            laptop_info['post_id'] = post_id
-            post_url = result_row.h3.a['href']
-            laptop_info['post_url'] = post_url
-            price = result_row.find('span', 'result-price').text
-            laptop_info['price'] = price
-            location = result_row.find('span', 'result-hood').text if result_row.find('span', 'result-hood') else ''
-            laptop_info['location'] = location
-            post_title = result_row.h3.a.text
-            laptop_info['post_title'] = post_title
-            search_results.append([
-                post_datetime, post_id, post_url, price, location, post_title
-            ])
+        post_datetime = result_row.time['datetime']
+        laptop_info['post_datetime'] = post_datetime
+        post_id = result_row.h3.a['data-id']
+        laptop_info['post_id'] = post_id
+        post_url = result_row.h3.a['href']
+        laptop_info['post_url'] = post_url
+        price = result_row.find('span', 'result-price').text
+        laptop_info['price'] = price
+        location = result_row.find('span', 'result-hood').text if result_row.find('span', 'result-hood') else ''
+        laptop_info['location'] = location
+        post_title = result_row.h3.a.text
+        laptop_info['post_title'] = post_title
+        search_result_per_page.append([
+            post_datetime, post_id, post_url, price, location, post_title
+        ])
 
-            add_laptop_data(post_id, post_title, price, post_datetime, location, post_url)
-            print(laptop_info)
+        add_laptop_data(post_id, post_title, price, post_datetime, location, post_url)
+        print(laptop_info)
+
+    return search_result_per_page
+
+
+def main_program(base_url):
+    search_results_all = []
+    print(f"current_url : {base_url}")
+
+    total_pages = get_total_pages(base_url)
+
+    for page_number in range(0, total_pages):
+        search_on_page_list = get_laptop_list(page_number, base_url)
+        search_results_all.extend(search_on_page_list)
         time.sleep(1)
 
     columns = ('Post Date', 'Post Id', 'Post Url', 'Price', 'Location', 'Post title')
-    df = pd.DataFrame(search_results, columns=columns)
+    df = pd.DataFrame(search_results_all, columns=columns)
+
     timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
-    df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
+    df.to_csv(f"{FILE_PATH}/craigslist_results_{timestamp}.csv", index=False)
     print('File Exported')
 
 
@@ -131,4 +145,7 @@ def main(query_url):
 
 if __name__ == "__main__":
     url = 'https://sfbay.craigslist.org/search/sfc/sya'
+    start_time = datetime.datetime.now()
     main(url)
+    end_time = datetime.datetime.now()
+    print(f"Total_run_time = {end_time - start_time}")
